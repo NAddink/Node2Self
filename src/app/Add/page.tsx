d55f6a2015147217b2908c322f1e8f24ef244ab6
Noah Addink <noahaddink@gmail.com>
2025-10-28 13:53:48 -0700

    Add connection form (not connected to db yet)

--- src/app/Add/page.tsx
+++ src/app/Add/page.tsx
@@ -1,6 +1,42 @@
+"use client";
+import { useState, ChangeEvent } from "react";
+import nodeExists from "../components/db";
 import Navbar from "../components/Navbar";
 
 export default function Add() {
+
+    const [firstName, setFirstName] = useState(''); // Initialize state for first name
+    const [lastName, setLastName] = useState(''); // Initialize state for last name
+
+    const firstNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setFirstName(event.target.value); // Update state on every input change
+        console.log("DEBUG: Current first name " + firstName)
+    };
+
+    const lastNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setLastName(event.target.value); // Update state on every input change
+        console.log("DEBUG: Current last name " + lastName)
+    };
+    
+
+    const addHandler = () => {
+
+        const fullname = firstName.trim() + " " + lastName.trim();
+        console.log(`DEBUG: Entered name is \'${fullname}\'`)
+
+        nodeExists(firstName.trim() + " " + lastName.trim())
+        .then(result => {
+            console.log(result);
+            if(result) { // entered node already exists
+                // only create connection to node
+            }
+            if(!result){ // entered node does not exist
+                // create node and create connection from current
+            }
+            
+        })
+        .catch
+    }
     
 
 
@@ -10,7 +46,31 @@ export default function Add() {
                 {/* Navbar */}
                 <Navbar />
                 
-                <h1>ADD</h1>
+                <div className="pt-5">
+                    <label className="text-black">First Name</label>
+                    <input 
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center" 
+                    type="text" 
+                    placeholder="John" 
+                    value={firstName} // Bind input value to state
+                    onChange={firstNameChange} // Update state on change
+                    />
+
+                    <label className="text-black">Last Name</label>
+                    <input 
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center" 
+                    type="text" 
+                    placeholder="Deer" 
+                    value={lastName} // Bind input value to state
+                    onChange={lastNameChange} // Update state on change
+                    />
+                    <br /> <br />
+                    
+
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer" type="button" onClick={addHandler}>
+                        Add Connection
+                    </button>
+                </div>
 
             </div>
         </div>
--- src/app/components/db.tsx
+++ src/app/components/db.tsx
@@ -1,7 +1,34 @@
 import axios from 'axios';
 
 
-function nodeExists {
+async function nodeExists (name : string) {
+    
+    if(name === "")
+    {
+        console.log("No node given as arg");
+        return false;
+    }
+
+    const response = await axios.get(`../api/nodes/${name}`);
+    console.log(response.data);
+
+    const length = response.data.length;
+    console.log(length);
+
+    if(response.data.length === 0)
+    {
+        console.log("Node does not exist")
+        return false;
+    }
+
+    if(response.data.length === 1)
+    {
+        console.log("Node exists")
+        return true;
+    }
+
+    return false;
 
 }
 
+export default nodeExists;
