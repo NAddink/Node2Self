c6a45bcb783752c7036d6d4c5637f685201d6a2f
Noah Addink <noahaddink@gmail.com>
2025-10-27 19:40:26 -0700

    GET, POST, DELETE for links and nodes working

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -21,3 +21,79 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ error: 'Failed to fetch links' }, { status: 500 });
   }
 }
+
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { source, target, added_by} = body;
+    if (!source || !target || !added_by) {
+      return NextResponse.json({ error: 'Missing required link fields' }, { status: 400 });
+    }
+
+    const pool = getPool();
+    const client = await pool.connect();
+    try {
+      await client.query('BEGIN');
+      const linkRes = await client.query(
+        `INSERT INTO links (source, target, added_by)
+         VALUES ($1, $2, $3) RETURNING id`,
+        [source, target, added_by]
+      );
+
+      const linkId: number = linkRes.rows[0].id;
+
+      
+
+      await client.query('COMMIT');
+      return NextResponse.json({ id: linkId }, { status: 201 });
+    } catch (err) {
+      await client.query('ROLLBACK');
+      console.error('POST /api/links transaction error:', err);
+      return NextResponse.json({ error: 'Error creating node' }, { status: 500 });
+    } finally {
+      client.release();
+    }
+  } catch (error) {
+    console.error('POST /api/links parse error:', error);
+    return NextResponse.json({ error: 'Invalid JSON body' }, { status: 400 });
+  }
+}
+
+export async function DELETE(request: NextRequest){
+    try {
+    
+
+    const pool = getPool();
+    const client = await pool.connect();
+    const url = new URL(request.url);
+    const linkIdParam = url.searchParams.get('id');
+
+    if (!linkIdParam) {
+      return NextResponse.json({ error: 'Missing required link fields' }, { status: 400 });
+    }
+
+
+    try {
+      await client.query('BEGIN');
+      const linkRes = await client.query(
+        `DELETE FROM links WHERE id = ($1)`,
+        [linkIdParam]
+      );
+      
+      
+
+      await client.query('COMMIT');
+      return new NextResponse(null, { status: 204 });
+    } catch (err) {
+      await client.query('ROLLBACK');
+      console.error('DELETE /api/links transaction error:', err);
+      return NextResponse.json({ error: 'Error deleting link' }, { status: 500 });
+    } finally {
+      client.release();
+    }
+  } catch (error) {
+    console.error('DELETE /api/links parse error:', error);
+    return NextResponse.json({ error: 'Invalid JSON body' }, { status: 400 });
+  }
+}
